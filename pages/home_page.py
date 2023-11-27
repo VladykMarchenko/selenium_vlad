f08a8fcd78fbc32456d53db2cbd1b75acdaaacff
vladmarchenko <vladyslavmarchenko@orderry.com>
2023-11-27 15:22:07 +0200

    dada

--- pages/home_page.py
+++ pages/home_page.py
@@ -9,15 +9,15 @@ class LoginPage:
 
     @property
     def username_input_field(self):
-        return self.driver.find_element(By.ID, 'login')
+        return self.driver.find_element(By.XPATH, '//*[@id="login"]')
 
     @property
     def password_input_field(self):
-        return self.driver.find_element(By.ID, 'password')
+        return self.driver.find_element(By.XPATH, '//*[@id="password"]')
 
     @property
     def login_button(self):
-        return self.driver.find_element(By.CSS_SELECTOR, '[type=submit] > div')
+        return self.driver.find_element(By.XPATH, '//div[@id="app"]//form//button')
 
     def setting_menu_group(self):
         self.driver.find_element(By.CSS_SELECTOR, '[data-cid="settings_menu_group"]').click()
@@ -37,9 +37,16 @@ class LoginPage:
     def click_subscription(self):
         self.driver.find_element(By. CSS_SELECTOR, '[data-cid="payments_menu_group"]').click()
 
+    # def incorrect_login_or_password(self):
+    #     self.driver.find_element(By. CSS_SELECTOR, '[data-cid="error-login"]')
+
     def incorrect_login_or_password(self):
-        self.driver.find_element(By. CSS_SELECTOR, '[data-cid="error-login"]')
+        error_message_element = self.driver.find_element(By.CSS_SELECTOR, '[data-cid="error-login"]')
+        return error_message_element.text
 
-
-
-
+    def is_displayed_incorrect_login_or_password(self):
+        try:
+            error_message_element = self.driver.find_element(By.CSS_SELECTOR, '[data-cid="error-login"]')
+            return error_message_element.is_displayed()
+        except NoSuchElementException:
+            return False
--- test_notifi_invalid_login.py
+++ test_notifi_invalid_login.py
@@ -3,6 +3,7 @@ from pages.home_page import LoginPage
 def test_notifi_invalid_login(driver) -> None:
     invalid_user = "invalid.user123@gmail1.comm"
     invalid_password = "invalid.user123@gmail1.comm"
+    expected_error_message = "Incorrect login or password"
 
     login_page = LoginPage(driver)
     login_page.navigate()
@@ -13,5 +14,11 @@ def test_notifi_invalid_login(driver) -> None:
     driver.implicitly_wait(10)
     login_page.incorrect_login_or_password()
     assert "Incorrect login or password" in driver.page_source
+    assert login_page.is_displayed_incorrect_login_or_password(), "Error message not displayed"
+    assert expected_error_message in login_page.incorrect_login_or_password(), f"Expected error message '{expected_error_message}' not found in page source"
 
-    driver.quit()
+
+# EC is_visible добавить в тест done
+# убрать проперти из хом пейдж
+# EC вместо implicitly_wait
+# navigate как отдельную централизованый метод отдельно вынести
